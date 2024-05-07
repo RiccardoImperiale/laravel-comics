a36ce88f068a3612b9d1b541d58c2aa91cd1ceff
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-07 16:07:29 +0200

    add current series section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,24 @@
 @extends('layouts.app')
-
 @section('content')
-    <section class="container">
-        <h1>Homepage</h1>
-        <p>Welcome to the homepage...</p>
+    {{-- @dd($comics) --}}
+    <section id="homepage">
+        <div class="container">
+            <div class="title_box">CURRENT SERIES</div>
+            <div class="comics">
+                @foreach ($comics as $comic)
+                    <div class="comic">
+                        <div class="comic_image">
+                            <img src="{{ $comic['thumb'] }}" alt="">
+                        </div>
+                        <div class="comic_title">{{ $comic['series'] }}</div>
+                    </div>
+                @endforeach
+
+            </div>
+            <div class="btn">LOAD MORE</div>
+        </div>
     </section>
+    <div class="banner container">
+
+    </div>
 @endsection

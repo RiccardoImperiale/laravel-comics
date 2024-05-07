72f5686364ccf0bd984fdc4bd85eebca99fd6f02
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-07 17:27:55 +0200

    add footer text

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,54 @@
 <footer>
     <div class="top_banner">
         <img src="{{ Vite::asset('resources/images/dc-logo-bg.png') }}" alt="">
+        <div class="container">
+            <div class="text">
+                <div class="link_lists">
+                    <div class="list">
+                        <div class="list_title">DC COMICS</div>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Video</li>
+                            <li>News</li>
+                        </ul>
+                    </div>
+                    <div class="list">
+                        <div class="list_title">DC</div>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy Policy</li>
+                            <li>Ad Choices</li>
+                            <li>Adverticing</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshop</li>
+                            <li>Rating</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                    <div class="list">
+                        <div class="list_title">SITES</div>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD MAgazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="copy">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum a error provident consequuntur
+                    nostrum. Blor sit, amet consectetur adipisicing elit. Rerum a error provident consequuntur
+                    nostrum.
+                </div>
+            </div>
+        </div>
     </div>
     <div class="bottom">
         <div class="container">

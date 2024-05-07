06d34d540e832df2287ac4bcd60d8f1e6110ea9c
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-07 17:12:03 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,30 @@
 <footer>
-    <div class="container">
-        <small>Random copyright text &copy;</small>
+    <div class="top_banner">
+        <img src="{{ Vite::asset('resources/images/dc-logo-bg.png') }}" alt="">
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <button>SIGN UP NOW</button>
+            <div class="socials">
+                <div>FOLLOW US</div>
+                <ul>
+                    <li>
+                        <img src="{{ Vite::asset('resources/images/footer-facebook.png') }}" alt="facebook icon">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/images/footer-periscope.png') }}" alt="periscope icon">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}" alt="pinterest icon">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/images/footer-twitter.png') }}" alt="twitter icon">
+                    </li>
+                    <li>
+                        <img src="{{ Vite::asset('resources/images/footer-youtube.png') }}" alt="youtube icon">
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
 </footer>

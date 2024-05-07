12d709ee01e6805d9aa51a2f2b7cabbc8d3c832d
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-07 15:09:48 +0200

    add header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,33 @@
 <header>
-    <nav class="container">
-        <div>
-            <a class="logo" href="{{ route('home') }}">Laravel</a>
+    <nav>
+        <div class="top">
+            <div class="container">
+                <ul>
+                    <li>DC POWER VISA &reg;</li>
+                    <li>ADDITIONAL DC SITES <span class="triangle">&#9660;</span></li>
+                </ul>
+            </div>
         </div>
-        <ul>
-            <li class="products_btn"><a href="{{ route('products') }}">Products</a></li>
-        </ul>
+        <div class="container">
+            <div class="bottom">
+                <img width="60" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo dc">
+                <ul>
+                    <li><a href="#">CHARACTERS</a></li>
+                    <li><a href="#">COMICS</a></li>
+                    <li><a href="#">MOVIES</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">GAMES</a></li>
+                    <li><a href="#">COLLECTIBLES</a></li>
+                    <li><a href="#">VIDEOS</a></li>
+                    <li><a href="#">FANS</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">SHOP <span class="triangle">&#9660;</span></a></li>
+                </ul>
+                <div class="input">
+                    <input type="text" placeholder="Search">
+                    <img width="16" src="{{ Vite::asset('resources/images/m-glass.png') }}" alt="magnifying glass">
+                </div>
+            </div>
+        </div>
     </nav>
 </header>
